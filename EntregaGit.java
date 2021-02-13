c2e3641bdcd23c124ae68c65a57c6a1f06f45000
etamura07 <emerson.tamura07@gmail.com>
2021-02-12 21:17:34 -0300

    Terminei, vou entregar

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class EntregaGit{
   public static void main(String[] args) {
-    System.out.println("Entregando tarefas com o git na linha de comando.");
+    String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+    // System.out.println("Entregando tarefas com o git na linha de comando.");
+    JOptionPane.showMessageDialog(null, String.format("Bem vindo, %s", nome));
   }
 }
